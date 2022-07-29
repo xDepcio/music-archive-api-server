2d4167db61b5aec3d8d8d6653f66b59d307bebfc
xDepcio <107278893+xDepcio@users.noreply.github.com>
2022-07-29 23:25:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -342,6 +342,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-const port = 5001;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => console.log('Server is listening on port', port));
